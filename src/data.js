5df0017d42e2df3c0d199b9f66492d638148a493
abhidevlopment <abhidevlopment4@gmail.com>
2019-08-24 23:34:11 +0530

    commited by abhi

---
+++ src/data.js
@@ -0,0 +1,171 @@
+export const placeInfo = [
+  {
+    id: 1,
+    headerTitle: 'Гараж',
+    headerSubTitle: 'Музей современного искусства',
+    headerText:
+      'Территория современного искусства в Парке Горького с образовательным центром, выставками, летним павильоном и кафе',
+    title: 'Музей современного искусства Гараж',
+    description:
+      '«Вся история существования Музея современного искусства «Гараж» тесно связана с архитектурой. В 2008 году мы реконструировали заброшенное здание Бахметьевского автобусного парка – памятника архитектуры советского авангарда, в 2012 году построили выставочный павильон в Парке Горького, который стоит в ряду исключительных примеров временной архитектуры, а в 2015 году открыли наше первое собственное здание, работу над которым мы вели с одним из самых важных архитекторов нашего времени – с Ремом Колхасом. Я уверена, что результатом нашего сотрудничества стало ярчайшее архитектурное сооружение, отвечающее мировым музейным требованиям, и нам, команде Гаража, остается только вдохнуть новую жизнь в это некогда заброшенное здание.»',
+    img: '.././images/garaj.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
+  },
+  {
+    id: 2,
+    headerTitle: 'Мамм',
+    headerSubTitle: 'музей',
+    headerText:
+      'Регулярные выставки современного искусства и мультимедийных проектов',
+    title: 'Мамм',
+    description:
+      'В 2009 году на базе Московского дома фотографии открылся Мультимедиа Арт Музей, Москва, который уже два года регулярно организует выставки современного искусства и мультимедийных проектов. Также к музею относится Школа фотографии и мультимедиа им. А. Родченко, среди преподавателей которой лучшие российские специалисты в области теории и практики фотографии и мультимедиа в России.',
+    img: '.././images/mamm.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1530.8494832689219!2d37.59693065124095!3d55.74157162835216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547845790316'
+  },
+  {
+    id: 3,
+    headerTitle: 'Пушкина',
+    headerSubTitle: 'Музей',
+    headerText:
+      'Второй по значению после Эрмитажа, государственный музей изобразительных искусств',
+    title:
+      'Второй по значению после Эрмитажа, государственный музей изобразительных искусств',
+    description:
+      'Музей занимает огромную площадь, сравнимую может только с Политехническим. В основном залы заполнены скульптурой и живописью. Представленные скульптуры по большей части копии, оригиналы хранятся в музеях Лондона, Ватикана и других.',
+    img: '.././images/pushkin.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17967.866965087615!2d37.60335432286895!3d55.74141791432578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb9384e1a3b2da434!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3Ri9C5INC80YPQt9C10Lkg0JAu0KEuINCf0YPRiNC60LjQvdCw!5e0!3m2!1sru!2sru!4v1547878731704'
+  },
+  {
+    id: 4,
+    headerTitle: 'Третьяковка',
+    headerSubTitle: 'Галерея',
+    headerText: 'Крупнейший художественный музей в Москве',
+    title: 'Крупнейший художественный музей в Москве',
+    description:
+      'Государственная Третьяковская галерея — художественный музей в Москве, основанный в 1856 году купцом Павлом Третьяковым и имеющий одну из самых крупных в мире коллекций русского изобразительного искусства.',
+    img: '.././images/tretyakovka.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9848620916428!2d37.618675215617316!3d55.741392000339864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afe32078357%3A0xa50d6e8b9a7b028f!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3QsNGPINCi0YDQtdGC0YzRj9C60L7QstGB0LrQsNGPINCz0LDQu9C10YDQtdGP!5e0!3m2!1sru!2sru!4v1547878659681'
+  },
+  {
+    id: 5,
+    headerTitle: 'Дом фотографии',
+    headerSubTitle: 'Фотоцентр',
+    headerText: 'Первый российский музей, специализирующийся в фотографи',
+    title: 'Первый российский музей, специализирующийся в фотографи',
+    description:
+      'Московский Дом фотографии располагает большой коллекцией старинной и современной российской художественной фотографии.',
+    img: '.././images/dom-fotografii.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9763498372804!2d37.59654121561725!3d55.741539900328505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547878587877'
+  },
+  {
+    id: 6,
+    headerTitle: "М'АРС",
+    headerSubTitle: 'Центр современного искусства',
+    headerText:
+      'Представляют искусство разнообразное, всегда отмеченное общей печатью оригинальности',
+    title:
+      'Представляют искусство разнообразное, всегда отмеченное общей печатью оригинальности',
+    description:
+      'Галерея М’АРС устраивает выставки, соединяя абсолютно разные темы, жанры, настроения, стили, вырываясь из плена правильности, являя свет ночи и музыку тишины.',
+    img: '.././images/mars.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2244.397087990157!2d37.62463341561838!3d55.76897499821247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a68e2e342b7%3A0x934bc1e9948c826b!2z0KbQtdC90YLRgCDQnNCw0YDRgQ!5e0!3m2!1sru!2sru!4v1547878491776'
+  }
+];
+
+export const reviews = [
+  {
+    id: 1,
+    name: 'Генадий',
+    avatar: '.././avatars/avatar-1.png',
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.'
+  },
+  {
+    id: 2,
+    name: 'Сергей',
+    avatar: '.././avatars/avatar-2.png',
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.'
+  },
+  {
+    id: 3,
+    name: 'Даша',
+    avatar: '.././avatars/avatar-3.png',
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.'
+  },
+  {
+    id: 4,
+    name: 'Александр',
+    avatar: '.././avatars/avatar-4.png',
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.'
+  },
+  {
+    id: 5,
+    name: 'Наталья',
+    avatar: '.././avatars/avatar-5.png',
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.'
+  }
+];
+
+export const detailInfo = {
+  id: 1,
+  headerTitle: 'Гараж',
+  headerSubTitle: 'Музей современного искусства',
+  headerText:
+    'Территория современного искусства в Парке Горького с образовательным центром, выставками, летним павильоном и кафе',
+  title: 'Музей современного искусства Гараж',
+  description:
+    '«Вся история существования Музея современного искусства «Гараж» тесно связана с архитектурой. В 2008 году мы реконструировали заброшенное здание Бахметьевского автобусного парка – памятника архитектуры советского авангарда, в 2012 году построили выставочный павильон в Парке Горького, который стоит в ряду исключительных примеров временной архитектуры, а в 2015 году открыли наше первое собственное здание, работу над которым мы вели с одним из самых важных архитекторов нашего времени – с Ремом Колхасом. Я уверена, что результатом нашего сотрудничества стало ярчайшее архитектурное сооружение, отвечающее мировым музейным требованиям, и нам, команде Гаража, остается только вдохнуть новую жизнь в это некогда заброшенное здание.»',
+  img: '.././images/garaj.jpg',
+  maps:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
+};
+
+export const news = [
+  {
+    id: 1,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  },
+  {
+    id: 2,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  },
+  {
+    id: 3,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  },
+  {
+    id: 4,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  },
+  {
+    id: 5,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  },
+  {
+    id: 6,
+    newsTitle: 'Lorem ipsum',
+    newsText:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure maiores optio praesentium voluptate natus velit atque eaque eos quae perspiciatis. Perferendis reprehenderit aspernatur excepturi sint saepe quam laboriosam provident ducimus.'
+  }
+];
